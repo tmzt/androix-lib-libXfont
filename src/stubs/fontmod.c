cc824e4f2c9a53a00b36a6f83bf065c363027087
Alan Coopersmith <alan.coopersmith@sun.com>
2007-03-23 15:57:29 -0700

    Actually use loadable font modules
    Loadable font modules were not being initialized, and all font renderers
    known at build time were always being initialized, regardless of Xorg
    module configuration.

---
+++ src/stubs/fontmod.c
@@ -0,0 +1,10 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#ifdef LOADABLEFONTS
+#include "stubs.h"
+#include <X11/fonts/fontmod.h>
+
+weak FontModule *FontModuleList;
+#endif /* LOADABLEFONTS */
