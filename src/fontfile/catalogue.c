f33f9361086db3cf1d09cec067f38fe751ed22eb
Kristian Høgsberg <krh@redhat.com>
2007-07-02 14:59:11 -0400

    Fix crash when no attributes are present for symlink.

--- src/fontfile/catalogue.c
+++ src/fontfile/catalogue.c
@@ -81,8 +81,13 @@ ComparePriority(const void *p1, const void *p2)
 {
     FontDirectoryPtr dir1 = (*(FontPathElementPtr*) p1)->private;
     FontDirectoryPtr dir2 = (*(FontPathElementPtr*) p2)->private;
-    const char *pri1 = strstr(dir1->attributes, PriorityAttribute);
-    const char *pri2 = strstr(dir2->attributes, PriorityAttribute);
+    const char *pri1 = NULL;
+    const char *pri2 = NULL;
+
+    if (dir1->attributes != NULL)
+	pri1 = strstr(dir1->attributes, PriorityAttribute);
+    if (dir2->attributes != NULL)
+	pri2 = strstr(dir2->attributes, PriorityAttribute);
 
     if (pri1 == NULL && pri2 == NULL)
 	return 0;
